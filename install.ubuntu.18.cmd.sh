414ae9851c3735cbd00a5548d5625f4ba0820a0f
sudao <sudao-b@rongcloud.cn>
2019-05-11 09:23:08 +0800

    update config

--- install.ubuntu.18.cmd.sh
+++ install.ubuntu.18.cmd.sh
@@ -18,13 +18,26 @@
 #sudo pip install shadowsocks
 
 #sudo pip install genpac
-pushd .
-cd shadowsocks
-sudo genpac --proxy="SOCKS5 127.0.0.1:1080" --gfwlist-proxy="SOCKS5 127.0.0.1:1080" -o autoproxy.pac --gfwlist-url="https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt"
-
-pop
+#pushd .
+## config shadowsocks http://www.totorocyx.me/2018/10/02/ubuntu_shadowsocks/
+#cd shadowsocks
+#sudo genpac --proxy="SOCKS5 127.0.0.1:1080" --gfwlist-proxy="SOCKS5 127.0.0.1:1080" -o autoproxy.pac --gfwlist-url="https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt"
+#sudo genpac --proxy="SOCKS5 127.0.0.1:1080" -o autoproxy.pac --gfwlist-url="https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt"
+#sudo apt-get install privoxy
+#popd
 
 #sudo apt install wine
+#sudo wine  Si3583Setup.exe
+#source insight license SI3US-304576-57379
+
+
+#install git lfs : refer https://www.jianshu.com/p/7d78c68c90d5
+sudo apt install yum
+#if below command fail, run it manually in terminal   curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+curl -s 'https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh' | sudo bash
+sudo apt-get install git-lfs
+git lfs install
+
 
 #=====================================================
 # vt-x bios : enable Intel Virtual tech In bios for Android emulator, 
